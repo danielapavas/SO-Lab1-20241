5d69384be90e3e2589cb96a05e63a0ea555f46ec
Daniela Pavas <daniela.pavas1@udea.edu.co>
2024-03-10 01:08:11 -0500

    Documentación faltante y ajustes finales

--- reverse.c
+++ reverse.c
@@ -15,17 +15,24 @@ void reversePrint(struct Node *head, FILE *outputFile);
 void freeList(struct Node *head);
 
 int main(int argc, char *argv[]) {
+    // Declaración de variables para manejo de archivos
     FILE *inputFile = NULL;
     FILE *outputFile = NULL;
+
+    // Declaración de estructuras y variables para generación de lista ligada
     struct Node *head = NULL;
     struct Node *current = NULL;
-    char *line = NULL;
+    char *line = NULL; 
+
+    // Declaración de variables para lectura de archivos
     size_t len = 0;
     ssize_t read;
+
+    //Declaración de variables para información de comando stat
     struct stat inputStat, outputStat;
     int err;
 
-    // Verificación de argumentos de línea de comandos
+    // Verificación de cantidad de argumentos en la línea de comandos
     if (argc > 3) {
         fprintf(stderr, "usage: reverse <input> <output>\n");
         exit(1);
@@ -38,35 +45,39 @@ int main(int argc, char *argv[]) {
             fprintf(stderr, "reverse: cannot open file '%s'\n", argv[1]);
             exit(1);
         }
-
+        
+        // Llamado a función stat para obtener el inode del archivo de entrada
         err = stat(argv[1], &inputStat);
         if (err < 0) {
-            perror("stat");
+            fprintf(stderr, "error: executing stat function for input file");
             exit(1);
         }
     } else {
         inputFile = stdin; // Utilizar la entrada estándar si no se especifica archivo de entrada
     }
 
-    if (argc == 3) {
-        // Abrir el archivo de salida si se especifica
+    // Validaciones en caso de ejecución con archivos de entrada y salida
+    if (argc == 3 ) {
+       
+        // Abrir el archivo de salida
         outputFile = fopen(argv[2], "w");
         if (outputFile == NULL) {
             fprintf(stderr, "error: cannot open file '%s'\n", argv[2]);
-            freeList(head); // Liberar la memoria de la lista enlazada antes de salir
             exit(1);
         }
 
+        // Llamado a función stat para obtener el inode del archivo de salida
         err = stat(argv[2], &outputStat);
         if (err < 0) {
-            perror("stat");
+            fprintf(stderr,"error: executing stat function for output file");
             exit(1);
         }
 
         if (strcmp(argv[1], argv[2]) == 0 || inputStat.st_ino == outputStat.st_ino) {
             fprintf(stderr, "reverse: input and output file must differ\n");
-            exit(EXIT_FAILURE);
+            exit(1);
         }
+        
     } else {
         outputFile = stdout; // Utilizar la salida estándar si no se especifica archivo de salida
     }
@@ -78,9 +89,12 @@ int main(int argc, char *argv[]) {
             fprintf(stderr, "malloc failed\n");
             exit(1);
         }
+
+        // Inicializa atributos de estructura del nodo creado
         newNode->line = strdup(line); // Almacenar la línea en el nodo
         newNode->next = NULL;
 
+        // Generando la lista ligada
         if (head == NULL) {
             head = newNode;
             current = newNode;
